84133e1d5bf10d8f9167b41730bb34089ccd11a4
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-05-11 16:29:32 -0700

    new project, VCS note

--- main.py
+++ main.py
@@ -2,3 +2,5 @@
 # author: Alexander Hungerford
 # description: first python script
 
+# when you create a new project with PyCharm you can add VCS
+# VCS > Import into Version Control > Share Project on Github

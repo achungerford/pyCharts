a6a1cc8b47ec6e976d624f6b23cfc00cb92f4e5a
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-06-23 11:12:36 -0700

    start over

--- main.py
+++ main.py
@@ -5,17 +5,3 @@
 # when you create a new project with PyCharm you can add VCS
 # VCS > Import into Version Control > Share Project on Github
 
-# Math: multiply, raise to power
-print("")
-print("multiply operator *, and power operator **\n")
-result = "Result: "
-
-# demo: 5 * 2
-print("5 * 2")
-print(result)
-print(5*2)
-print("")
-
-print("5 ** 2")
-print(result)
-print(5 ** 2)

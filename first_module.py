28f30b0e21cf23faf6f0770ef47b79727ba7ae13
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-06-29 00:43:17 -0700

    modules

---
+++ first_module.py
@@ -0,0 +1,8 @@
+# You can store functions you write in a separate file called a module.
+# Then you can import that module into your main program.
+
+# An import statement tells Python to make the code available
+#       in the currently running program file.
+
+# This is my first attempt at creating a module.
+
